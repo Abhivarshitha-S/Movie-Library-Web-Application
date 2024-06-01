6c7116d9d8b23ade55f1032302453a1f08121d2e
Abhivarshitha <116748885+Abhivarshitha-S@users.noreply.github.com>
2024-06-01 11:25:52 +0530

    Update Home.js

---
+++ Home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Movie Library</h1>
+      <MovieSearch />
+      <MovieList />
+      <CreateList />
+      {/* Add other components or functionality here */}
+    </div>
+  );
+}
